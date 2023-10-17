2dd4bcbce6c715b43888e1c9367db1f5c6dd9107
Raphael Izidro <148289580+raphaelizidro@users.noreply.github.com>
2023-10-17 20:34:03 -0300

    Delete nodejs directory

--- nodejs/object.js
+++
@@ -1,9 +0,0 @@
-let friend ={name: 'José',  
-sex: 'M',
-weight: 85.4,
-gains(p=0){
-    console.log('gain weight')
-    this.weight += p
-}}
-console.log(`${friend.name} weights ${friend.weight}Kg`)
-
