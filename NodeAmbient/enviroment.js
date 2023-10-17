83e393b17e89183796db46346c1b9b170048df5d
Raphael Izidro <148289580+raphaelizidro@users.noreply.github.com>
2023-10-17 20:31:46 -0300

    Add files via upload

---
+++ NodeAmbient/enviroment.js
@@ -0,0 +1,18 @@
+var c = 1 
+while (c <= 6){
+    console.log('Tudo Bem?')
+    c++
+}
+
+
+
+
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
