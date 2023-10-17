f374ba93be42275d5877d408a9c38353173eacb7
Raphael Izidro <148289580+raphaelizidro@users.noreply.github.com>
2023-10-17 20:36:30 -0300

    Add files via upload

---
+++ Nodejs/nodejs/object.js
@@ -0,0 +1,9 @@
+let friend ={name: 'José',  
+sex: 'M',
+weight: 85.4,
+gains(p=0){
+    console.log('gain weight')
+    this.weight += p
+}}
+console.log(`${friend.name} weights ${friend.weight}Kg`)
+
