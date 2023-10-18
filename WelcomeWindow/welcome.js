1919679f0a92e5437443e49aefa48d66702911d8
Raphael Izidro <148289580+raphaelizidro@users.noreply.github.com>
2023-10-17 21:33:50 -0300

    Update welcome.js

--- WelcomeWindow/welcome.js
+++ WelcomeWindow/welcome.js
@@ -2,7 +2,7 @@ function start(){
 
  let name = window.prompt('What is your name?')
  let age = window.prompt(` Ok, ${name}, how old are you?`)
- let finalresult = window.alert(`I met ${name} with ${age} years old.`)
+ let finalresult = window.alert(`Welcome ${name} with ${age} years old.`)
     
 }
 
