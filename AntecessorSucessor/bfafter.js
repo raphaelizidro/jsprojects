135ef38203a0e559f246248cae579928cee29e2d
Raphael Izidro <148289580+raphaelizidro@users.noreply.github.com>
2023-10-18 20:26:55 -0300

    Update bfafter.js

--- AntecessorSucessor/bfafter.js
+++ AntecessorSucessor/bfafter.js
@@ -2,5 +2,5 @@ function start(){
     let n = window.prompt('Type any number')
     let ant = (n - 1)
     let suc = (ant + 2)
-    let finalresult = window.prompt(`O antecessor de ${n} é ${ant} e o sucessor é ${suc}`)
+    let finalresult = window.alert(`O antecessor de ${n} é ${ant} e o sucessor é ${suc}`)
 }
